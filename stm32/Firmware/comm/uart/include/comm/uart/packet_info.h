b11131f49468cc8ea35577a03f2627877f929768
Hayden Mai <144507076+haydenmai@users.noreply.github.com>
2025-11-03 13:30:03 -0800

    Packet info additions and edits - Added CRC8 calculation function - Renamed most of ePacketID enums

--- stm32/Firmware/comm/uart/include/comm/uart/packet_info.h
+++ stm32/Firmware/comm/uart/include/comm/uart/packet_info.h
@@ -2,7 +2,7 @@
  * @file packet_info.h
  * @brief Contains relevant information about UART data packets
  * @author Hayden Mai
- * @date Oct-31-2025
+ * @date Nov-03-2025
  */
 
 #ifndef COMM_UART_PACKET_INFO_H_
@@ -10,46 +10,51 @@
 
 #include <array>
 #include <cstdint>
+#include <cstddef>
 
 namespace uart {
-    /** @brief List of IDs to/from the mcu */
-    enum class ePacketID : uint8_t {
-        // Receiving (STM32 -> Radxa)
-        TELEMETRY,    // Contains sensor (imu, ultrasonic, encoder), pid(s) data
-        STATUS_STM32, // Status of the STM32
-        BATTERY,      // Measured battery voltage
-        ACK_STM32,    // Confirm receipt from STM32
-        DEBUG,        // Debugging log
-
-        // Transmitting (Radxa -> STM32)
-        CMD_MOTOR,        // Motor control
-        CMD_NAV,          // Target speed, turn, start/stop
-        CONFIG_PID_SPEED, // Tune speed PID
-        CONFIG_PID_LANE,  // Tune laning PID
-        CONFIG_SENSOR,    // Configure sensor data rate
-        STATUS_RADXA,     // Status of the Radxa
-        ACK_RADXA,        // Confirm receipt from Radxa
-    };
-
-
     // Max data packet size
-    constexpr size_t DATA_MAX_SIZE {256};
+    constexpr size_t DATA_MAX_SIZE {255};
 
     // Sync bytes
     constexpr uint8_t SYNC_RECV {0x5A};
     constexpr uint8_t SYNC_SEND {0xA5};
 
+
+    /** @brief List of IDs to/from the mcu */
+    enum class ePacketID : uint8_t {
+        // Receiving (STM32 -> Radxa)
+        TELEM_IMU,     // IMU data
+        TELEM_ULT,     // Ultrasonic data
+        TELEM_ENC,     // Encoder data
+        TELEM_PID,     // Contains pid data
+        TELEM_BATTERY, // Measured battery voltage
+        STM32_STATUS,  // Status of the STM32
+        STM32_ACK,     // Confirm receipt from STM32
+        STM32_DEBUG,   // Debugging log
+
+        // Transmitting (Radxa -> STM32)
+        CMD_MOTOR,      // Motor control
+        CMD_NAV,        // Target speed, turn, start/stop
+        CONF_PID_SPEED, // Tune speed PID
+        CONF_PID_LANE,  // Tune laning PID
+        CONF_SENSOR,    // Configure sensor data rate
+        RAD_STATUS,     // Status of the Radxa
+        RAD_ACK,        // Confirm receipt from Radxa
+    };
+
+
     /** @brief Raw data packet structure from reading UART */
     struct DataPacket_raw {
-        uint8_t sync {};       // Header - 0x5A (Radxa receive) or 0xA5 (Radxa transmit)
-        ePacketID id {};       // Refer to ePacketID enum class
-        uint32_t timestamp {}; // millisecs since program start, big enough for ~49.7 days
-        uint8_t length {};     // Max bits length of data: 255 bytes
+        uint8_t sync {};   // Header - 0x5A (Radxa receive) or 0xA5 (Radxa transmit)
+        ePacketID id {};   // Refer to ePacketID enum class
+        uint8_t length {}; // Max bits length of data: 255 bytes
         uint8_t data[DATA_MAX_SIZE]; // Data
                                      // CRC8 at data[length]
 
-        size_t totalSize() const { return 7 + length + 1; }
+        size_t totalSize() const { return 3 + length + 1; }
     } __attribute__((packed));
+
 
     namespace recv {
         /** @brief IMU data structure from the telemetry packet */
@@ -62,6 +67,10 @@ namespace uart {
             int16_t gyro_z {};
         };
     } // namespace recv
+
+
+    /** @brief Calculate CRC8 checksum */
+    uint8_t calculate_crc8(uint8_t *data, uint16_t length);
 
 } // namespace uart
 
