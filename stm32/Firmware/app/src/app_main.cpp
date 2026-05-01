6b6cabc12a3daa6d0cd5896029addfe01ec55d32
Michael <106024119+TheMuskFan@users.noreply.github.com>
2026-05-01 16:55:59 -0700

    imu code working

--- stm32/Firmware/app/src/app_main.cpp
+++ stm32/Firmware/app/src/app_main.cpp
@@ -2,13 +2,14 @@
  * @file app_main.cpp
  * @brief Initialize modules needed. Should be called from Core/main.c
  * @author Hayden Mai
- * @date Apr-29-2026
+ * @date May-01-2026
  */
 
 #include "app/app_main.h"
 #include "comm/uart/manager.h"
 
 #include "cmsis_os.h"
+#include "hal/imu.h"
 #include "main.h"
 
 // External global variables from Core/Src/main.c
@@ -28,14 +29,20 @@ extern "C" void app_main(void)
 {
     // initialize other modules and start new threads and stuff
     // call C++ functions here
-    uart::manager::init(&huart1, uart::manager::eUARTInstance::UART_1);
-    uart::manager::start();
+    // uart::manager::init(&huart1, uart::manager::eUARTInstance::UART_1);
+    // uart::manager::start();
+    std::array<float, 3> a_data;
+    IMU MPU6050(&hi2c1, 0x68);
+    MPU6050.scan_i2c();
 
     while (true) {
-        uart::send::enqueue_debug("Debug here!");
+        /* uart::send::enqueue_debug("Debug here!");
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
         osDelay(150);
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-        osDelay(2000);
+        osDelay(2000); */
+        MPU6050.get_accel(a_data);
+        printf("x =%f, y=%f , z=%f\n", a_data[0], a_data[1], a_data[2]);
+        vTaskDelay(500);
     }
 }
---
+++ stm32/Firmware/hal/src/imu.cpp
@@ -0,0 +1,77 @@
+#include "hal/imu.h"
+#include "stm32f4xx_hal_i2c.h"
+#include <array>
+
+IMU::IMU(I2C_HandleTypeDef *handle, int address) : hi2c_(handle), address_(address) {}
+
+void IMU::scan_i2c()
+{
+    printf("Starting I2C Scan\n");
+
+    // Go through all the possible I2C addresses
+    for (uint8_t i = 0; i < 128; i++) {
+        if (HAL_I2C_IsDeviceReady(hi2c_, (uint16_t)(i << 1), 3, 5) == HAL_OK)
+            printf("%2x", i);
+        else
+            printf("-- ");
+
+        // new line every 16 addresses
+        if (i > 0 && (i + 1) % 16 == 0)
+            printf("\n");
+    }
+
+    printf("\n");
+}
+
+void IMU::get_accel_raw(std::array<int, 3> &accel)
+{
+    uint8_t data[6];
+    HAL_I2C_Mem_Read(hi2c_, address_ << 1, ACCEL_XOUT_H, 1, data, 6, HAL_MAX_DELAY);
+    int16_t raw_ax = (int16_t)((data[0] << 8) | data[1]);
+    int16_t raw_ay = (int16_t)((data[2] << 8) | data[3]);
+    int16_t raw_az = (int16_t)((data[4] << 8) | data[5]);
+
+    accel[0] = raw_ax;
+    accel[1] = raw_ay;
+    accel[2] = raw_az;
+}
+
+void IMU::get_accel(std::array<float, 3> &accel)
+{
+    uint8_t data[6];
+    HAL_I2C_Mem_Read(hi2c_, address_ << 1, ACCEL_XOUT_H, 1, data, 6, HAL_MAX_DELAY);
+    int16_t raw_ax = (int16_t)((data[0] << 8) | data[1]);
+    int16_t raw_ay = (int16_t)((data[2] << 8) | data[3]);
+    int16_t raw_az = (int16_t)((data[4] << 8) | data[5]);
+
+    // divide by sensivity to get g units
+    accel[0] = raw_ax / ACCEL_SENSITIVITY;
+    accel[1] = raw_ay / ACCEL_SENSITIVITY;
+    accel[2] = raw_az / ACCEL_SENSITIVITY;
+}
+
+void IMU::get_gyro_raw(std::array<int, 3> &gyro)
+{
+    uint8_t data[6];
+    HAL_I2C_Mem_Read(hi2c_, address_ << 1, GYRO_XOUT_H, 1, data, 6, HAL_MAX_DELAY);
+    int16_t raw_gx = (int16_t)((data[0] << 8) | data[1]);
+    int16_t raw_gy = (int16_t)((data[2] << 8) | data[3]);
+    int16_t raw_gz = (int16_t)((data[4] << 8) | data[5]);
+
+    gyro[0] = raw_gx;
+    gyro[1] = raw_gy;
+    gyro[2] = raw_gz;
+}
+
+void IMU::get_gyro(std::array<float, 3> &gyro)
+{
+    uint8_t data[6];
+    HAL_I2C_Mem_Read(hi2c_, address_ << 1, GYRO_XOUT_H, 1, data, 6, HAL_MAX_DELAY);
+    int16_t raw_gx = (int16_t)((data[0] << 8) | data[1]);
+    int16_t raw_gy = (int16_t)((data[2] << 8) | data[3]);
+    int16_t raw_gz = (int16_t)((data[4] << 8) | data[5]);
+
+    gyro[0] = raw_gx / GYRO_SENSITIVITY;
+    gyro[1] = raw_gy / GYRO_SENSITIVITY;
+    gyro[2] = raw_gz / GYRO_SENSITIVITY;
+}
