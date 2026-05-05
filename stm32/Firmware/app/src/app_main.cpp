6098d68f0ea9cebc41974622db78230ba6df65bc
Hayden Mai <haydenmaihm@gmail.com>
2026-05-05 14:48:35 -0700

    Updated main for UART testing

--- stm32/Firmware/app/src/app_main.cpp
+++ stm32/Firmware/app/src/app_main.cpp
@@ -2,7 +2,7 @@
  * @file app_main.cpp
  * @brief Initialize modules needed. Should be called from Core/main.c
  * @author Hayden Mai
- * @date May-03-2026
+ * @date May-04-2026
  */
 
 #include "app/app_main.h"
@@ -11,6 +11,8 @@
 #include "cmsis_os.h"
 #include "hal/imu.h"
 #include "main.h"
+
+#include <cstdio>
 
 // External global variables from Core/Src/main.c
 // NOTE: Only pass them as reference through initialization for modules/classes,
@@ -31,15 +33,22 @@ extern "C" void app_main(void)
     // call C++ functions here
     uart::manager::init(&huart1, uart::manager::eUARTInstance::UART_1);
     uart::manager::start();
-    //std::array<float, 3> a_data;
-    //IMU MPU6050(&hi2c1, 0x68);
-    //MPU6050.scan_i2c();
+    uint32_t debugCounter {1};
+    // std::array<float, 3> a_data;
+    // IMU MPU6050(&hi2c1, 0x68);
+    // MPU6050.scan_i2c();
 
     while (true) {
-        uart::send::enqueue_debug("Debug here!");
+        char debugMessage[32] {};
+        std::snprintf(debugMessage, sizeof(debugMessage), "Debug here! #%lu",
+                      static_cast<unsigned long>(debugCounter++));
+        uart::send::enqueue_debug(debugMessage);
+        osDelay(50);
+        uart::send::enqueue_ack();
+
         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-        //MPU6050.get_accel(a_data);
-        //printf("x =%f, y=%f , z=%f\n", a_data[0], a_data[1], a_data[2]);
-        vTaskDelay(500);
+        // MPU6050.get_accel(a_data);
+        // printf("x =%f, y=%f , z=%f\n", a_data[0], a_data[1], a_data[2]);
+        osDelay(50);
     }
 }
