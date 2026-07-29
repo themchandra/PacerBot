7fc790cdfa9e99bc717ffa79b42c25d75446dea3
Michael <106024119+TheMuskFan@users.noreply.github.com>
2026-07-29 13:20:05 -0700

    stm-side test for uart

--- stm32/Firmware/app/src/app_main.cpp
+++ stm32/Firmware/app/src/app_main.cpp
@@ -2,7 +2,7 @@
  * @file app_main.cpp
  * @brief Initialize modules needed. Should be called from Core/main.c
  * @author Hayden Mai
- * @date Jul-16-2026
+ * @date Jul-29-2026
  */
 
 #include "app/app_main.h"
@@ -11,6 +11,7 @@
 
 #include "comm/uart/callbacks.h"
 #include "comm/uart/manager.h"
+#include "comm/uart/recv.h"
 
 #include "cmsis_os.h"
 #include "main.h"
@@ -32,30 +33,22 @@ extern TIM_HandleTypeDef htim3;
 
 extern "C" void app_main(void)
 {
-    // Peripherals & Hardware sensors
     uart::manager::init(&huart1, uart::manager::eUARTInstance::UART_1);
-
-    // App layer inits
-    app::ControlLoop control_loop(&htim3, &huart6);
-    app::CMDManager cmd_manager(control_loop);
-
-    // Start tasks loops
     uart::manager::start();
-    cmd_manager.start();
-    control_loop.start();
 
-    control_loop.set_target_speed(2.0f);
-    osDelay(15000);
-    // control_loop.set_target_speed(20.0f);
-    // osDelay(10000);
-    control_loop.set_target_speed(0.0f);
+    uart::DataPacket_raw packet {};
 
-    // Main 'idle' loop
     while (true) {
-        // Sign-of-life LED Blinking
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-        osDelay(100);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-        osDelay(4900);
+        if (uart::recv::dequeue(&packet, osWaitForever)) {
+            if (packet.id == uart::ePacketID::HOST_DEBUG) {
+                printf("Received from Linux: ");
+
+                for (uint8_t i = 0; i < packet.length; ++i) {
+                    printf("%c", packet.data[i]);
+                }
+
+                printf("\r\n");
+            }
+        }
     }
 }
