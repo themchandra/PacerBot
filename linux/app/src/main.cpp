6983dedb35278a684f55f7a6a8e4d5ef2f1719d1
Michael Chandra <106024119+themchandra@users.noreply.github.com>
2026-07-29 13:18:09 -0700

    working test for linux->stm

--- linux/app/src/main.cpp
+++ linux/app/src/main.cpp
@@ -1,28 +1,33 @@
 #include "comm/uart/manager.h"
+#include "comm/uart/send.h"
 
 #include <array>
+#include <chrono>
 #include <cstdint>
 #include <iostream>
+#include <thread>
 
-/**
- * Minimal UART test matching the STM32 app_main:
- * send a HOST_DEBUG packet and wait for its STM32_ACK.
- */
 int main()
 {
-
+    std::cout << "Initializing UART\n";
     uart::manager::init();
-    auto ack = uart::recv::subscribe({uart::ePacketID::STM32_ACK});
+
+    std::cout << "Starting UART manager\n";
     uart::manager::start();
 
     constexpr std::array<uint8_t, 16> message {
-        'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r', 'o', 'm', ' ', 'L', 'i', 'n', 'u', 'x'
+        'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r',
+        'o', 'm', ' ', 'L', 'i', 'n', 'u', 'x'
     };
+
+    std::cout << "Enqueuing HOST_DEBUG packet\n";
     uart::send::enqueue(
         uart::DataPacket(uart::ePacketID::HOST_DEBUG, message));
 
-    ack->pop();
-    std::cout << "Received ACK from STM32\n";
+    // Give the background send thread time to process the queue.
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::cout << "Packet queued\n";
 
     uart::manager::stop();
     uart::manager::deinit();
