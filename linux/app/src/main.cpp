c59b1546e762bdc8887862c7adcfb183ef912ff0
Michael Chandra <106024119+themchandra@users.noreply.github.com>
2026-07-29 13:34:44 -0700

    stm->beagle test

--- linux/app/src/main.cpp
+++ linux/app/src/main.cpp
@@ -1,33 +1,24 @@
 #include "comm/uart/manager.h"
-#include "comm/uart/send.h"
 
-#include <array>
-#include <chrono>
-#include <cstdint>
 #include <iostream>
-#include <thread>
 
 int main()
 {
     std::cout << "Initializing UART\n";
     uart::manager::init();
 
+    auto sub = uart::recv::subscribe({
+        uart::ePacketID::STM32_ACK
+    });
+
     std::cout << "Starting UART manager\n";
     uart::manager::start();
 
-    constexpr std::array<uint8_t, 16> message {
-        'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r',
-        'o', 'm', ' ', 'L', 'i', 'n', 'u', 'x'
-    };
+    std::cout << "Waiting for STM32_ACK...\n";
 
-    std::cout << "Enqueuing HOST_DEBUG packet\n";
-    uart::send::enqueue(
-        uart::DataPacket(uart::ePacketID::HOST_DEBUG, message));
+    auto packet = sub->pop();
 
-    // Give the background send thread time to process the queue.
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    std::cout << "Packet queued\n";
+    std::cout << "Received STM32_ACK\n";
 
     uart::manager::stop();
     uart::manager::deinit();
