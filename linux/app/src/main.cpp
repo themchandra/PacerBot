cd5a2978a6e3305369a0f9f09217bcb8ba6a8243
Michael Chandra <106024119+themchandra@users.noreply.github.com>
2026-07-29 20:10:19 -0700

    working main loop linux side for manual control

--- linux/app/src/main.cpp
+++ linux/app/src/main.cpp
@@ -1,25 +1,25 @@
 #include "comm/uart/manager.h"
-
+#include "comm/uart/packet_info.h"
+#include "comm/uart/send.h"
+#include "manual_control.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main()
 {
-    std::cout << "Initializing UART\n";
     uart::manager::init();
-
-    auto sub = uart::recv::subscribe({
-        uart::ePacketID::STM32_ACK
-    });
-
-    std::cout << "Starting UART manager\n";
     uart::manager::start();
+    ManualControl control;
 
-    std::cout << "Waiting for STM32_ACK...\n";
-
-    auto packet = sub->pop();
-
-    std::cout << "Received STM32_ACK\n";
-
+    while (true) {
+        updateManualControl(control);
+        const auto encoded = encodeManualControl(control);
+        const std::array<uint8_t, 1> payload { encoded };
+        uart::send::enqueue(uart::DataPacket(uart::ePacketID::CMD_MCTL,payload));
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
+    
     uart::manager::stop();
     uart::manager::deinit();
 
